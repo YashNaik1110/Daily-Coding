218c5b60557f4bf198a8fe4d3e14d4a0ba1aaa25
Yash Naik <yashn949@gmail.com>
2026-07-13 00:02:34 +0530

    Add files via upload

---
+++ ASCIIValue.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class ASCIIValue{
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter Any charachter");
+         char ch = sc.next().charAt(0);
+
+        int ascii = (int)ch;
+        System.out.println("ASCII Value of "+ch +" : " + ascii);
+    }
+}
