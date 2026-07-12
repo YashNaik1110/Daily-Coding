d632f3679155fef195d6e28519f946cc0b71a7e6
Yash Naik <yashn949@gmail.com>
2026-07-12 19:56:34 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,8 @@
+class Average{
+    public static void main(String[] args){
+        float a = 12.0f ;
+        float b = 15.0f ;
+        float average = a+b%2;
+        System.out.println("Avergae of Number : " + average);
+    }
+}
