73e3e3cd20265617d8d2435eb4ce8347effe1725
Yash Naik <yashn949@gmail.com>
2026-07-12 20:01:29 +0530

    Add files via upload

---
+++ SwapTwoNum.java
@@ -0,0 +1,14 @@
+class SwapTwoNum{
+    public static void main(String[] args) {
+      int a = 1 ;
+      int b  =2 ;
+      int c = a ;
+      a = b ;
+      b = c ;
+      
+      
+     System.out.println("a = "+ a);
+    System.out.println("b = "+  b);
+
+    }
+}
