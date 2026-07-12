ccbba0fe7f88588466e5d0665ce4f3ded346042a
Yash Naik <yashn949@gmail.com>
2026-07-12 20:31:54 +0530

    Add files via upload

---
+++ ConvFarenToCels.java
@@ -0,0 +1,7 @@
+class ConvFarenToCels{
+    public static void main(String[] args) {
+        double far = 71.6 ;
+        double cel = (far-32)*5/9;
+        System.out.println("Converted Farenhit to celsius : "+ cel ); 
+    }
+}
