60706e2ee2f861c98ca12468f9af6e708cba8ac5
Yash Naik <yashn949@gmail.com>
2026-07-13 00:52:17 +0530

    Update NumberCheck.java

--- NumberCheck.java
+++ NumberCheck.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 class NumberCheck{
   public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a Number : ");
       int num = sc.nextInt();
 
       if(num<0){
