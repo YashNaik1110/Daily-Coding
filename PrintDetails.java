2135441f474063a481ec16be9f88d9deb77a8b11
Yash Naik <yashn949@gmail.com>
2026-07-12 19:46:08 +0530

    Add files via upload

---
+++ PrintDetails.java
@@ -0,0 +1,12 @@
+class PrintDetails{
+    public static void main(String[] args) {
+
+        String name = "Yash Naik ";
+        int age = 21 ;
+        String city = "Pune" ;
+      System.out.println("Name : " + name);
+      System.out.println("Age  : "   + age);
+      System.out.println("City :  " + city);
+
+    }
+}
