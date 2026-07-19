74a53093d7e080ca6073ee0d4c448700d73810e5
Yash Naik <yashn949@gmail.com>
2026-07-19 20:44:52 +0530

    Array

---
+++ AllElements.java
@@ -0,0 +1,9 @@
+class AllElements{
+	public static void main(String[] args) {
+		int[] arr = {1334 , 1111 , 1233 , 1456};
+
+		for(int i = 0 ; i<arr.length ; i++){
+			System.out.println(arr[i]);
+		}
+	}
+}
